28d5ff219f4c72137c27aad291c0e15dbe3ab0bc
Trevor Elliott <trevor@galois.com>
2015-03-21 17:27:39 -0700

    Restrict exports

--- Data/SBV/Examples/CodeGeneration/LLVM.hs
+++ Data/SBV/Examples/CodeGeneration/LLVM.hs
@@ -10,6 +10,6 @@ test = compileToLLVM Nothing "foo" $
            let r = ite (bnot (a .> 10))
                      (((a + 32) `shiftL` 10) `shiftR` 20)
                      (abs (a - (10 :: SWord32)))
-            in (bar b + (r # r)) :: SWord64
+            in rotateR ((bar b + (r # r)) :: SWord64) 10
 
      cgReturn (f arr)
